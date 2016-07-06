712dcb76592503f3a2f9b0022ed3175bed906b34
David Armbrust <david.armbrust@gmail.com>
2016-07-05 21:36:28 -0500

    37/282

--- about_arrays.rb
+++ about_arrays.rb
@@ -31,7 +31,10 @@ class AboutArrays < Neo::Koan
     assert_equal :jelly, array[-1]
     assert_equal :butter, array[-3]
   end
-
+#When slicing and array the position after the last index is still 
+  #considered part of the array and will return [] when a length is
+  #used.
+  #if only this position is called it will return nil
   def test_slicing_arrays
     array = [:peanut, :butter, :and, :jelly]
 
@@ -40,45 +43,49 @@ class AboutArrays < Neo::Koan
     assert_equal [:and, :jelly], array[2,2]
     assert_equal [:and, :jelly], array[2,20]
     assert_equal [], array[4,0]
-    assert_equal __, array[4,100]
-    assert_equal __, array[5,0]
+    assert_equal [], array[4,100]
+    assert_equal nil, array[5,0]
   end
-
+  #.. is inclusive
+  #... is not inclusive
   def test_arrays_and_ranges
-    assert_equal __, (1..5).class
-    assert_not_equal [1,2,3,4,5], (1..5)
-    assert_equal __, (1..5).to_a
-    assert_equal __, (1...5).to_a
+    assert_equal Range, (1..5).class
+    assert_not_equal [1,2,3,4], (1..5)
+    assert_equal [1,2,3,4,5], (1..5).to_a
+    assert_equal [1,2,3,4], (1...5).to_a
   end
-
+  #using a negative at the end of the range will go up to the referenced
+  #index. ie -1 will go up to the last index, -2 will go to the second 
+  #to last
   def test_slicing_with_ranges
     array = [:peanut, :butter, :and, :jelly]
 
-    assert_equal __, array[0..2]
-    assert_equal __, array[0...2]
-    assert_equal __, array[2..-1]
+    assert_equal [:peanut, :butter, :and], array[0..2]
+    assert_equal [:peanut, :butter], array[0...2]
+    assert_equal [:and, :jelly], array[2..-1]
   end
 
   def test_pushing_and_popping_arrays
     array = [1,2]
     array.push(:last)
 
-    assert_equal __, array
+    assert_equal [1, 2, :last], array
 
     popped_value = array.pop
-    assert_equal __, popped_value
-    assert_equal __, array
+    assert_equal :last, popped_value
+    assert_equal [1, 2], array
   end
-
+  # Unshift will add an item to the begining of an array, Shift will
+  # return the first value in an array and remove it
   def test_shifting_arrays
     array = [1,2]
     array.unshift(:first)
 
-    assert_equal __, array
+    assert_equal [:first, 1, 2], array
 
     shifted_value = array.shift
-    assert_equal __, shifted_value
-    assert_equal __, array
+    assert_equal :first, shifted_value
+    assert_equal [1, 2], array
   end
 
 end
